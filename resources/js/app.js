f2697b8a43088a0d18812d094de6902bd7918e7a
Andrew Eason <aeason96@vt.edu>
2016-06-10 16:41:42 -0400

    got movement working kinda

---
+++ resources/js/app.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+          var pos = $('#p1').position();
+          pos.left-=5;
+          $('#p1').css('left', pos.left);
+        break;
+
+        case 38: // up
+          var pos = $('#p1').position();
+          pos.top-=5;
+          $('#p1').css('top', pos.top);
+        break;
+
+        case 39: // right
+          var pos = $('#p1').position();
+          pos.left+=5;
+          $('#p1').css('left', pos.left)
+        break;
+
+        case 40: // down
+          var pos = $('#p1').position();
+          pos.top+=5;
+          $('#p1').css('top', pos.top);
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+});
