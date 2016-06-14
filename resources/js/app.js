2d88e38de05b4f6acfc2ab478d211136d126663c
Andrew Eason <aeason96@vt.edu>
2016-06-14 09:30:10 -0400

    smoother movement

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,127 +1,36 @@
 
-$(document).ready(function(){
-  $(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-          var pos = $('#p1').position();
-          pos.left-=5;
-          $('#p1').css('left', pos.left);
-        break;
-
-        case 38: // up
-          var pos = $('#p1').position();
-          pos.top-=5;
-          $('#p1').css('top', pos.top);
-        break;
-
-        case 39: // right
-          var pos = $('#p1').position();
-          pos.left+=5;
-          $('#p1').css('left', pos.left)
-        break;
-
-        case 40: // down
-          var pos = $('#p1').position();
-          pos.top+=5;
-          $('#p1').css('top', pos.top);
-        break;
-        
-        case 13:
-          var posi = $('#p1').position();
-          cat("andrew");
-          alert("" + posi.left + ", " + posi.top);
-
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-  });
-
-  $('.square').click(function(){
-    var posi = $(this).position();
-    alert("" + posi.left + ", " + posi.top);
-  });
-  /**
   var keys = {};
-
+  setInterval(printKeys, 50);
   $(document).keydown(function (e) {
       keys[e.which] = true;
-
-      move();
   });
-
   $(document).keyup(function (e) {
       delete keys[e.which];
-
-      move();
   });
-
-  function move() {
+  function printKeys() {
       var html = '';
       for (var i in keys) {
-        if (!keys.hasOwnProperty(i)) continue;
-        var pos = $('#p1').position();
-        switch(i) {
-          case 37: // left
-            var pos = $('#p1').position();
-            pos.left-=5;
-            $('#p1').css('left', pos.left);
-          break;
-
-          case 38: // up
-            var pos = $('#p1').position();
-            pos.top-=5;
-            $('#p1').css('top', pos.top);
-          break;
-
-          case 39: // right
-            var pos = $('#p1').position();
-            pos.left+=5;
-            $('#p1').css('left', pos.left)
-          break;
-
-          case 40: // down
-            var pos = $('#p1').position();
-            pos.top+=5;
-            $('#p1').css('top', pos.top);
-          break;
-
-          case 13:
-            var posi = $('#p1').position();
-            alert("" + posi.left + ", " + posi.top);
-
-          default: return; // exit this handler for other keys
-        }
-        e.preventDefault(); // prevent the default action (scroll / move caret)
+          if (!keys.hasOwnProperty(i)) continue;
+          html += '<p>' + i + '</p>';
       }
-  }*/
-  function cat(name){
-    alert(name);
+      move();
+      $('#console').html(html);
   }
-});
-
-
-/**
-Print all keys currently pressed
-var keys = {};
-
-$(document).keydown(function (e) {
-    keys[e.which] = true;
-    
-    printKeys();
-});
-
-$(document).keyup(function (e) {
-    delete keys[e.which];
-    
-    printKeys();
-});
-
-function printKeys() {
-    var html = '';
-    for (var i in keys) {
-        if (!keys.hasOwnProperty(i)) continue;
-        html += '<p>' + i + '</p>';
+  function move(){
+    var pos = $('#p1').position();
+    for (var i in keys){
+      if (i == 38){
+        $('#p1').css('top', pos.top -= 5);
+      } 
+      if (i == 39){
+        $('#p1').css('left', pos.left += 5);
+      } 
+      if (i == 37){
+        $('#p1').css('left', pos.left -= 5);
+      } 
+      if (i == 40){
+        $('#p1').css('top', pos.top += 5);
+      } 
     }
-    $('#out').html(html);
-}
-*/
+  };
+
